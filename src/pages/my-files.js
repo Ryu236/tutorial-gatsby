6bff87fe33c8081aead663c7edc6cb49cdf85862
Ryu236 <ryu.adv.2360@gmail.com>
2020-02-01 19:36:55 +0900

    :sparkles: Feat(graphql): change my-file site to files table

--- src/pages/my-files.js
+++ src/pages/my-files.js
@@ -5,7 +5,29 @@ export default ({ data }) => {
   console.log(data)
   return (
     <Layout>
-      <div>Hello World!</div>
+      <div>
+        <h1>My Site's Files</h1>
+        <table>
+          <thread>
+            <tr>
+              <th>relativePath</th>
+              <th>prettySize</th>
+              <th>extension</th>
+              <th>birthTime</th>
+            </tr>
+          </thread>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.prettySize}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   )
 }
